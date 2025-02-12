5f6cf8cd94cde75d3101c157c06b9e711d237436
Ashan Kavindu <93070984+Ashan-TUF@users.noreply.github.com>
2025-02-12 23:20:49 +0530

    view categories
    view categories

---
+++ src/app/components/manage/categories/categories.component.ts
@@ -0,0 +1,49 @@
+import {Component, inject, ViewChild} from '@angular/core';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {CategoryService} from "../../../services/category.service";
+
+@Component({
+  selector: 'app-categories',
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule],
+  templateUrl: './categories.component.html',
+  styleUrl: './categories.component.scss'
+})
+export class CategoriesComponent {
+  displayedColumns: string[] = ['id', 'name', 'action'];
+  dataSource: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  categoryService = inject(CategoryService);
+
+  constructor() {
+    this.dataSource = new MatTableDataSource([] as any);
+  }
+
+  ngOnInit(){
+    this.categoryService.getCategories().subscribe((result:any)=>{
+      console.log(result);
+      this.dataSource.data=result;
+    })
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
